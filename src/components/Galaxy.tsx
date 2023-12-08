6c346c00a31ad8b73f96041d36964ac43448cf40
Ashley <ashleyoftexas@gmail.com>
2023-12-08 16:58:47 -0600

    favicon & styling

--- src/components/Galaxy.tsx
+++ src/components/Galaxy.tsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 
 const Galaxy = () => {
   const [galaxyText, setGalaxyText] = useState("");
-  const [fadeIn, setFadeIn] = useState(false);
 
   useEffect(() => {
     fetch("/Home/galaxy.txt")
@@ -10,13 +9,6 @@ const Galaxy = () => {
       .then((text) => setGalaxyText(text))
       .catch((error) => console.error("Error fetching galaxy:", error));
   }, []);
-
-  useEffect(() => {
-    setFadeIn(true);
-    return () => {
-      setFadeIn(false);
-    };
-  });
 
   return (
     <div className="galaxy container d-flex align-items-center justify-content-center">
