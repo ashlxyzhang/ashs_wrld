f69782c28d5e340ff55117aea53cfdde6b4452c0
Ashley <ashleyoftexas@gmail.com>
2024-01-02 19:23:56 -0600

    more galaxy formatting :(

--- src/components/Galaxy.tsx
+++ src/components/Galaxy.tsx
@@ -14,26 +14,38 @@ const Galaxy = () => {
 
   return (
     <div className="galaxy">
+      <img className="fade-in gal-img" src="/Home/galaxy2.jpeg" />
       <div className="d-flex fade-in vh-100 align-items-center justify-content-center">
-        <div className="d-flex flex-column title-cont">
-          <h1 className="space-text space-title">
-            Hello... I'm Ashley. <br />
-            It's nice to finally meet you.
+        <div className="d-flex space-text flex-column title-cont">
+          <h1 className="space-title">
+            Hello! ⭐️ I'm Ashley. <br />
+            It's nice to meet you.
           </h1>
-          <h5 className="space-text space-subtitle">
-            This website is an extension of my mind. <br />
-            Feel free to click around & enjoy.
+          <h3 className="space-mid">
+            I'm a current freshman engineering honors student at Texas A&M
+            University.
+          </h3>
+          <h5 className="space-subtitle">
+            This website is an extension of my mind as a galaxy.
+            <br />
+            Feel free to explore it & enjoy!
           </h5>
-          <div></div>
         </div>
-        <div className="d-flex gal-cont vh-100 align-items-center justify-content-center">
-          <img className="gal-img" src="/Home/galaxy2.jpeg" />
-          <span className="galaxy-text" style={{ marginRight: "5%" }}>
-            {galaxyText}
-          </span>
+        <span
+          className="space-text"
+          style={{
+            fontSize: 10,
+            textAlign: "center",
+            alignSelf: "end",
+            zIndex: 1,
+          }}
+        >
+          Made with 💛 by 🍎 <br />© 2024. All rights reserved.
+        </span>
+        <div className="d-flex vh-100 align-items-center justify-content-center">
           <div
             className="dest-cont"
-            style={{ marginBottom: "2%", marginRight: "5%" }}
+            style={{ marginBottom: "2%", marginRight: "7%" }}
           >
             <Link to="/" className="destination">
               🌎
