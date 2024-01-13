f8ed92f0804da35976e689118af6f987c989e521
Ashley <ashleyoftexas@gmail.com>
2024-01-12 22:29:16 -0600

    Revert "trying to deploy"
    This reverts commit 416ae4ac6791441d2d8db662ea9ac1fe57352b48.

--- src/components/Advent/Three.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-const Three = () => {
-    return _jsx("div", { className: "ascii" });
-};
-export default Three;
--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "bootstrap/dist/css/bootstrap.css";
-import "./App.css";
-ReactDOM.createRoot(document.getElementById("root")).render(_jsx(React.StrictMode, { children: _jsx(App, {}) }));
