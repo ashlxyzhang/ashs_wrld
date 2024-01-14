94825904b49cb4de95b3d018326f188a3598bbef
Ashley <ashleyoftexas@gmail.com>
2024-01-13 22:40:51 -0600

    source code clean up & routing for deployed website

--- src/components/Phonebook/Login.tsx
+++ src/components/Phonebook/Login.tsx
@@ -4,7 +4,7 @@ import controller from "./Tracks.tsx";
 function Login() {
   const id = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
   const secret = import.meta.env.VITE_SPOTIFY_CLIENT_SECRET;
-  const redir = "http://localhost:5173/auth/callback";
+  const redir = `https://ashs-wrld.web.app/auth/callback`;
   const scope = "user-top-read";
   const spotifyAuthUrl = `https://accounts.spotify.com/authorize?client_id=${id}&redirect_uri=${redir}&scope=${scope}&response_type=code`;
 
