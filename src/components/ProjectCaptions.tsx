8ad66b8f0f5f415eba8e7f0e318acaf9be64df2a
Ashley <ashleyoftexas@gmail.com>
2024-04-03 13:38:56 -0500

    project page clean up

---
+++ src/components/ProjectCaptions.tsx
@@ -0,0 +1,8 @@
+function getProjectCaptions() {
+  const caps = [
+    "Financial service integrated with Plaid API to help users track spending and promote financial education. Offers breakdown of net worth by assets and liabilities and by specific accounts like cash, investments, credit, and loans.",
+  ];
+  return caps;
+}
+
+export default getProjectCaptions;
